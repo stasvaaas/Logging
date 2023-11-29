14801ea8caa5e3fb67979f3c0fa41f287962374e
stasvaaas <105922844+stasvaaas@users.noreply.github.com>
2023-11-29 12:52:47 +0200

    Add project files.

---
+++ Logging/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Logging
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"D:\file.txt";
+
+            using (StreamWriter writer = new StreamWriter(filePath, true))
+            {
+                if (new FileInfo(filePath).Length == 0)
+                {
+                    writer.WriteLine($"First start at {DateTime.Now}");
+                }
+                else
+                {
+                    writer.WriteLine($"{DateTime.Now}");
+                }
+            }
+
+        }
+    }
+}
